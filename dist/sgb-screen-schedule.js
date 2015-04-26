5598a4dff41bb8745912a0fe95d1c1f6a3c49748
Saul Hernandez <sauldhernandez@gmail.com>
2015-04-26 13:37:01 -0430

    Added default for screen title

--- dist/sgb-screen-schedule.js
+++ dist/sgb-screen-schedule.js
@@ -3,7 +3,7 @@
 
 angular.module('sgb-screen-schedule', ['megazord'])
     .controller('sgb-screen-schedule-controller', ['$stateParams', '_screenParams', '$scope', function($stateParams, _screenParams, $scope){
-        $scope.title = _screenParams.title;
+        $scope.title = _screenParams.title || 'schedule_title';
         $scope.schedule = $stateParams.data;
     }]);
 
