4fdf92cc82988c7c3d4caf5eab11715a7fe1b033
Alexander A. Ramírez M <alexander.ramirez@synergy-gb.com>
2015-10-30 15:59:35 -0430

    Changed $stateParams to _data

--- dist/sgb-screen-schedule.js
+++ dist/sgb-screen-schedule.js
@@ -2,10 +2,10 @@
 'use strict';
 
 angular.module('sgb-screen-schedule', ['megazord'])
-    .controller('sgb-screen-schedule-controller', ['$stateParams', '_screen', '_screenParams', '$scope', function($stateParams, _screen, _screenParams, $scope){
+    .controller('sgb-screen-schedule-controller', ['_data', '_screen', '_screenParams', '$scope', function(_data, _screen, _screenParams, $scope){
         _screen.initialize($scope, _screenParams);
 
-        $scope.schedule = $stateParams.data;
+        $scope.schedule = _data;
     }]);
 
 
